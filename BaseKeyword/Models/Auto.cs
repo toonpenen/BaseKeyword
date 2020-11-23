2549af7925566463e023611ec7ee7491b57b82aa
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-23 09:58:47 +0100

    Add project files.

---
+++ BaseKeyword/Models/Auto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseKeyword.Models
+{
+    public class Auto
+    {
+        public string Type { get; set; }
+        public int NumberOfDoors { get; set; } = 4;
+
+    }
+}
